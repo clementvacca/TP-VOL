64bb3a6884c92ed4730a83cbd12ca58077059199
unknown <you@example.com>
2019-01-16 09:44:08 +0100

    Ajout CompAer et Reserv

--- src/main/java/tpVol/model/Reservation.java
+++ src/main/java/tpVol/model/Reservation.java
@@ -1,5 +1,54 @@
 package tpVol.model;
 
-public class Reservation {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="reservation")
+@SequenceGenerator(name = "seqReservation", sequenceName = "seq_reservation", allocationSize = 1, initialValue = 0)
+public class Reservation {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqReservation")
+	@Column(name="id_reservation")
+	private Long idReservation;
+	@Column(name="date_reservation")
+	private Date dateReservation;
+	@Column(name="numero_reservation")
+	private Integer numeroReservation;
+	
+	public Reservation() {
+	}
+
+	public Reservation(Date dateReservation, Integer numeroReservation) {
+		super();
+		this.dateReservation = dateReservation;
+		this.numeroReservation = numeroReservation;
+	}
+
+	public Date getDateReservation() {
+		return dateReservation;
+	}
+
+
+	public void setDateReservation(Date dateReservation) {
+		this.dateReservation = dateReservation;
+	}
+
+
+	public Integer getNumeroReservation() {
+		return numeroReservation;
+	}
+
+
+	public void setNumeroReservation(Integer numeroReservation) {
+		this.numeroReservation = numeroReservation;
+	}
+	
 }
