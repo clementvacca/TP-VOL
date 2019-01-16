424abcf915b5602d3855e318b3abc800586246b3
Your Name <you@example.com>
2019-01-16 17:04:36 +0100

    mise a jour aeroport

--- src/test/java/resources/AppTestLaurent.java
+++ src/test/java/resources/AppTestLaurent.java
@@ -12,12 +12,23 @@ public class AppTestLaurent {
 
 	public static void main(String[] args) {
 		Ville par=new Ville("paris");
+		Ville rdj=new Ville("rio de janeiro");
+		Ville mar=new Ville("marseille");
 		DaoVille daoVille=DaoVilleFactory.getInstance();
 		daoVille.insert(par);
+		daoVille.insert(rdj);
+		daoVille.insert(mar);
+		
 		Aeroport cdg=new Aeroport("charles de gaulles",par);
+		Aeroport gal=new Aeroport("galeo",rdj);
+		Aeroport mari=new Aeroport("marignane",mar);
+		Aeroport orl=new Aeroport("orly",par);
 		DaoAeroport daoAeroport=DaoAeroportFactory.getInstance();
 		
 		daoAeroport.insert(cdg);
+		daoAeroport.insert(gal);
+		daoAeroport.insert(mari);
+		daoAeroport.insert(orl);
 		
 		
 		
